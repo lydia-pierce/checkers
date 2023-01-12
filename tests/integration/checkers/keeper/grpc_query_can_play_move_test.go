c88a8f2d7cfd3369112a7abe45a4f32a3ea605fa
lydia.pierce@principia.edu <lydia.pierce@principia.edu>
2023-01-12 13:12:38 -0500

    denom unit and integration tests

--- tests/integration/checkers/keeper/grpc_query_can_play_move_test.go
+++ tests/integration/checkers/keeper/grpc_query_can_play_move_test.go
@@ -11,16 +11,16 @@ var canPlayOkResponse = &types.QueryCanPlayMoveResponse{
 }
 
 func (suite *IntegrationTestSuite) TestCanPlayAfterCreate() {
-    suite.setupSuiteWithOneGameForPlayMove()
-    goCtx := sdk.WrapSDKContext(suite.ctx)
-    response, err := suite.queryClient.CanPlayMove(goCtx, &types.QueryCanPlayMoveRequest{
-        GameIndex: "1",
-        Player:    "b",
-        FromX:     1,
-        FromY:     2,
-        ToX:       2,
-        ToY:       3,
-    })
-    suite.Require().Nil(err)
-    suite.Require().EqualValues(canPlayOkResponse, response)
+	suite.setupSuiteWithOneGameForPlayMove()
+	goCtx := sdk.WrapSDKContext(suite.ctx)
+	response, err := suite.queryClient.CanPlayMove(goCtx, &types.QueryCanPlayMoveRequest{
+		GameIndex: "1",
+		Player:    "b",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	suite.Require().Nil(err)
+	suite.Require().EqualValues(canPlayOkResponse, response)
 }
